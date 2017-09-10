95463130afaa840641a5c589f74b1dcaa56ee027
Brayden <btraas@gmail.com>
2017-09-10 15:42:45 -0700

    Initial commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Brayd
+ * Date: 9/7/2017
+ * Time: 2:16 PM
+ */
+
+?>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title></title>
+</head>
+<body>
+<?php
+echo "Hello out there ... I live";
+?>
+</body>
+</html>
